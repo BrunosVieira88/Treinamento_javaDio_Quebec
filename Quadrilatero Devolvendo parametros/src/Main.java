63080e5a3e232534784259c986637e3bc6c371d8
Bruno Santos Vieira <75911878+BrunosVieira88@users.noreply.github.com>
2022-09-12 21:37:28 -0300

    quadrilatero retorno

---
+++ Quadrilatero Devolvendo parametros/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        System.out.println("A aréa é igual a "+Quadrilatero.area(3));
+        System.out.println("Área do retângulo:"+Quadrilatero.area(5d,5d));
+        System.out.println("Área do trapézio:" +Quadrilatero.area(7,8,9));
+        System.out.println("Área do losango:" +Quadrilatero.area(5f,5f));
+
+    }
+}
